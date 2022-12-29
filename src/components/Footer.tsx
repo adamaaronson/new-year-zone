80e902dfcd51719a09e11e01d984f5a5040564ae
Adam Aaronson <adamaaronson@gmail.com>
2022-12-29 11:44:11 -0600

    Change hyphens to minus signs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
     return <footer className="footer">
         <span>© {new Date().getFullYear()} <a href="https://twitter.com/aaaronson" target="_blank" rel="noopener noreferrer">
             Adam</a> and <a href="https://twitter.com/larryaaronson" target="_blank" rel="noopener noreferrer">
-            Larry</a> Aaronson / Made in UTC-6 / <a href="https://twitter.com/larryaaronson" target="_blank" rel="noopener noreferrer">
+            Larry</a> Aaronson / Made in UTC−6 / <a href="https://twitter.com/larryaaronson" target="_blank" rel="noopener noreferrer">
             Learn more</a>
         </span>
     </footer>
